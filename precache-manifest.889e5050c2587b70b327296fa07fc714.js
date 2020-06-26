a628f7c85bf52e58faa280e7daa7c503830f0af2
Mike Keohane <mkeohane@gatech.edu>
2020-06-25 22:26:51 -0400

    Updates

---
+++ precache-manifest.889e5050c2587b70b327296fa07fc714.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87566c63787861a7ee5ba48ea283ecd6",
+    "url": "/PatentPlusInventionProfile/index.html"
+  },
+  {
+    "revision": "b5efea707da08245ceb7",
+    "url": "/PatentPlusInventionProfile/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3b876ec0cbca8214ecb4",
+    "url": "/PatentPlusInventionProfile/static/css/main.03b6626c.chunk.css"
+  },
+  {
+    "revision": "b5efea707da08245ceb7",
+    "url": "/PatentPlusInventionProfile/static/js/2.896dc318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PatentPlusInventionProfile/static/js/2.896dc318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b876ec0cbca8214ecb4",
+    "url": "/PatentPlusInventionProfile/static/js/main.57815689.chunk.js"
+  },
+  {
+    "revision": "87162884b8f175ecf269",
+    "url": "/PatentPlusInventionProfile/static/js/runtime-main.02ed0bc3.js"
+  },
+  {
+    "revision": "d00d0783d4269ea6343ae8526c26bfd4",
+    "url": "/PatentPlusInventionProfile/static/media/details-icon-default.d00d0783.png"
+  },
+  {
+    "revision": "fd51af73a9e64f170bfd51dce6bec5a8",
+    "url": "/PatentPlusInventionProfile/static/media/details-icon-selected.fd51af73.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PatentPlusInventionProfile/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f2cf80995e293f58d136c4479308cfdc",
+    "url": "/PatentPlusInventionProfile/static/media/navbar-logo.f2cf8099.png"
+  },
+  {
+    "revision": "b32009ee875708f1704899cbd83a2e8e",
+    "url": "/PatentPlusInventionProfile/static/media/overview-icon-default.b32009ee.png"
+  },
+  {
+    "revision": "587e2c4ca8857a551363bc83f6ef3c62",
+    "url": "/PatentPlusInventionProfile/static/media/overview-icon-selected.587e2c4c.png"
+  },
+  {
+    "revision": "484e4937de4093ba292c398e969148cf",
+    "url": "/PatentPlusInventionProfile/static/media/review-icon-default.484e4937.png"
+  },
+  {
+    "revision": "db06ce433ce4d480c9ff7ec5648ed37b",
+    "url": "/PatentPlusInventionProfile/static/media/review-icon-selected.db06ce43.png"
+  },
+  {
+    "revision": "84b848878e06325594b30083dd6ca266",
+    "url": "/PatentPlusInventionProfile/static/media/user-icon.84b84887.png"
+  }
+]);
